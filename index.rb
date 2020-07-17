79ec50acd367b0823ecfcfa0df429d72fd3c289d
takagotch <30462722+takagotch@users.noreply.github.com>
2020-07-18 02:14:27 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,8 +1,16 @@
-File.open('data.csv') do |file|
-  file.each_line do |data|
-    if data.chomp == "takagotch" then
-      puts data
-    end
-  end
-end
+# CSV.read
+
+filename = "appdata.csv"
+data = CSV.read(appdata, headers: true)
+puts data
+
+####
+#
+# File.open('data.csv') do |file|
+#  file.each_line do |data|
+#    if data.chomp == "takagotch" then
+#      puts data
+#    end
+#  end
+# end
 
